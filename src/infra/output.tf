4bac3aae309fe1b65e8c3d5c48dc870a6cb97e78
パダラパトラ ネール <nehru@dl-techx-xedge.net>
2026-07-04 16:21:38 +0900

    first commit

---
+++ src/infra/output.tf
@@ -0,0 +1,13 @@
+output "virtual_machine_id" {
+  value = module.avm-res-compute-virtualmachine.id
+}
+
+
+output "network_interface_id" {
+  value = module.avm-res-network-interface.id
+}
+
+
+output "subnet_id" {
+  value = module.avm-res-subnet.id
+}
