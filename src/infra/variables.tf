f645712d679c8162d11fdc11eff086b8dd530320
パダラパトラ ネール <nehru@dl-techx-xedge.net>
2026-07-04 19:14:19 +0900

    variables added vm name

--- src/infra/variables.tf
+++ src/infra/variables.tf
@@ -13,3 +13,10 @@ azurerm_virtual_network" "name" {
 }
 
 
+"azurerm_linux_virtual_machine" "name" {
+  description = "The name of the Linux virtual machine."
+  type        = string
+  default     = "example-machine"
+}
+
+
